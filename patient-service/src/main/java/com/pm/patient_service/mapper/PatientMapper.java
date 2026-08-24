4367e2e043ce6e009cfa1861902262d278ef250b
syamantak <132641600+Syamantak-Git@users.noreply.github.com>
2026-08-24 20:09:28 +0530

    refactor(mapper): rename method toDTO for clarity and consistency

--- patient-service/src/main/java/com/pm/patient_service/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patient_service/mapper/PatientMapper.java
@@ -1,10 +1,13 @@
 package com.pm.patient_service.mapper;
 
+import java.time.LocalDate;
+
+import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.model.Patient;
 
 public class PatientMapper {
-    public static PatientResponseDTO toPatientResponseDTO(Patient patient) {
+    public static PatientResponseDTO toDTO(Patient patient) {
         PatientResponseDTO dto = new PatientResponseDTO();
         dto.setId(patient.getId().toString());
         dto.setName(patient.getName());
@@ -12,5 +15,15 @@ public class PatientMapper {
         dto.setAddress(patient.getAddress());
         dto.setDateOfBirth(patient.getDateOfBirth().toString());
         return dto;
+    }
+
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
     }
 }
