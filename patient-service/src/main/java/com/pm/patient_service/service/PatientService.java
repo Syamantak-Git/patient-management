60d4764b46782e56fec109000e23e9301cd46f80
syamantak <132641600+Syamantak-Git@users.noreply.github.com>
2026-08-24 20:09:47 +0530

    fix(service): correct mapping method toDTO for patient response

--- patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
@@ -20,7 +21,13 @@ public class PatientService {
     public List<PatientResponseDTO> getAllPatients() {
         List<Patient> patients = patientRepository.findAll();
         List<PatientResponseDTO> patientResponseDTOs = patients.stream()
-                .map(PatientMapper::toPatientResponseDTO).toList();
+                .map(PatientMapper::toDTO).toList();
         return patientResponseDTOs;
+    }
+
+    public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        Patient patient = PatientMapper.toModel(patientRequestDTO);
+        patientRepository.save(patient);
+        return PatientMapper.toDTO(patient);
     }
 }
