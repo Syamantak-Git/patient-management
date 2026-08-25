0175e4e53263611b6ce109c089fee202184f9850
syamantak <132641600+Syamantak-Git@users.noreply.github.com>
2026-08-25 13:39:34 +0530

    feat(service): add email existence check in createPatient method

--- patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.exception.EmailAlreadyExistsException;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
@@ -26,6 +27,9 @@ public class PatientService {
     }
 
     public PatientResponseDTO createPatient(PatientRequestDTO patientRequestDTO) {
+        if (patientRepository.existsByEmail(patientRequestDTO.getEmail())) {
+            throw new EmailAlreadyExistsException("Patient with email " + patientRequestDTO.getEmail() + " already exists.");
+        }
         Patient patient = PatientMapper.toModel(patientRequestDTO);
         patientRepository.save(patient);
         return PatientMapper.toDTO(patient);
