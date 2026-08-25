4487698d56551036213e5497e6a3f1ab272c1a8c
syamantak <132641600+Syamantak-Git@users.noreply.github.com>
2026-08-25 14:57:42 +0530

    feat(repository): add method to check email existence excluding specific ID

--- patient-service/src/main/java/com/pm/patient_service/repository/PatientRepository.java
+++ patient-service/src/main/java/com/pm/patient_service/repository/PatientRepository.java
@@ -9,5 +9,6 @@ import com.pm.patient_service.model.Patient;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, UUID>{
+    boolean existsByEmailAndIdNot(String email, UUID id);
     boolean existsByEmail(String email);
 }
